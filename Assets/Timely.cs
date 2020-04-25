ca66f7d2a95ed8cdac6599db7b2858e054332fd5
Chobotov <chobotov@bk.ru>
2020-04-25 03:01:02 +0300

    Set a timely variant

---
+++ Assets/Timely.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timely : MonoBehaviour
+{
+    public GameObject first;
+    public GameObject second;
+    public GameObject third;
+
+    private void Start()
+    {
+        SaveLoadManager.Instance.playerData.currentUnits[0] = first;
+        SaveLoadManager.Instance.playerData.currentUnits[1] = second;
+        SaveLoadManager.Instance.playerData.currentUnits[2] = third;
+    }
+}
